1034cfcc1f59a7c6039753b26874943b0ea5d605
Enya Moncayo <glitzkipalys@gmail.com>
2026-07-29 21:45:27 +0800

    fix side bar on mobile

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -150,7 +150,7 @@ export default function Sidebar() {
           <ThemeToggle />
           <GlobalSearch />
           <button
-            onClick={() => setMobileOpen((o) => !o)}
+            onClick={toggleMobileOpen}
             aria-label="Toggle menu"
             className="w-9 h-9 rounded-lg border border-white/15 flex items-center justify-center flex-shrink-0"
           >
